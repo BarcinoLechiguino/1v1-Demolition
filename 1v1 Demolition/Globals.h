9bc10503e20e91af0ce0ccba6b69cc7da97ed69e
Gromeu2000 <45124641+Gromeu2000@users.noreply.github.com>
2019-12-15 20:11:21 +0100

    Update Globals.h

--- 1v1 Demolition/Globals.h
+++ 1v1 Demolition/Globals.h
@@ -47,7 +47,7 @@ enum update_status
 #define CAM_OFFSET 1
 
 // Vehicle Movement Variables
-#define MAX_ACCELERATION 5000.0f			//Fast: 5500.0f, Slow but steady: 3650.0f 
+#define MAX_ACCELERATION 5200.0f			//Fast: 5500.0f, Slow but steady: 3650.0f 
 #define TURN_DEGREES 35.0f * DEGTORAD		//Slow Turn: 25.0f
 #define BRAKE_POWER 1250.0f
 
