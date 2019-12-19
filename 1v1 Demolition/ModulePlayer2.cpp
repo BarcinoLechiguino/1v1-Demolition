a14fcb894fe9e22f3474521356214917120d4a29
Barcino <angotov@gmail.com>
2019-12-19 12:01:14 +0100

    Vehicle chassis and cabin resize.

--- 1v1 Demolition/ModulePlayer2.cpp
+++ 1v1 Demolition/ModulePlayer2.cpp
@@ -293,11 +293,11 @@ void ModulePlayer2::CheckLivesP2()
 
 void ModulePlayer2::GenerateP2Vehicle()
 {
-	// Car properties ----------------------------------------
+	// Car properties ----------------------------------------	
 	car.chassis2_size.Set(3.5f * scale, 1.0f * scale, 6.0f * scale);
 	car.chassis2_offset.Set(0.0f * scale, 0.7f * scale, 0.0f * scale);
 
-	car.cabin2_size.Set(3.9f * scale, 1.5f * scale, 3.0f * scale);
+	car.cabin2_size.Set(3.4f * scale, 1.5f * scale, 3.0f * scale);
 	car.cabin2_offset.Set(0.0f * scale, 1.2f * scale, -0.5f * scale);
 
 	car.leftLight2_size.Set(0.4f * scale, 0.2f * scale, 6.05f * scale);
